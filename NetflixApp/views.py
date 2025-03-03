559b6c2668177b1db318883e22fa683488a3968e
EMMMABK <adilbikiev2@gmail.com>
2025-03-03 20:43:35 +0600

    add New Commit

--- NetflixApp/views.py
+++ NetflixApp/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 # Create your views here.
 
 def index(request):
     return render(request, 'NetflixApp/index.html')
 
 def login(request):
-    return render(request, 'NetflixApp/login.html')
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, "NetflixApp/login.html", {"form": form})
 
 def signup(request):
-    return render(request, 'NetflixApp/signup.html')
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, "NetflixApp/signup.html", {"form": form})
