3a6c31df6f244f4164abc8d91c25c88629b7b0ce
EMMMABK <adilbikiev2@gmail.com>
2025-03-08 18:09:53 +0600

    add SignUp and Logout function

--- NetflixApp/urls.py
+++ NetflixApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
     path('signup/', views.signup, name='signup'),
 ]
--- NetflixApp/views.py
+++ NetflixApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 
@@ -18,19 +20,34 @@ def login(request):
         form = AuthenticationForm()
     return render(request, "NetflixApp/login.html", {"form": form})
 
+def user_logout(request):
+    logout(request)
+    return(redirect('/'))
+
 def signup(request):
-    errors = []
-    
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('index')
+    if request.method == 'POST':
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+                return redirect('/')
         else:
-            errors = [error for error_list in form.errors.values() for error in error_list]
+            messages.info(request, 'Password not matching')
+            return redirect('signup')
 
     else:
-        form = UserCreationForm()
-    
-    return render(request, "NetflixApp/signup.html", {"form": form, "errors": errors})
+        return render(request, 'NetflixApp/signup.html')
